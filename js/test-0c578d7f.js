f8775cbd5bb53dc24d2d8b87ed0df11db4d1a360
guo_hub <41369069+guohub8080@users.noreply.github.com>
2023-04-12 01:21:34 +0800

    Add files via upload

---
+++ js/test-0c578d7f.js
@@ -0,0 +1,5 @@
+import{d as a,o as l,a as c,c as i,b as u,t as n,u as F,e,F as d}from"./index-806ce535.js";const p=e("h1",null,"\u8FD9\u662F\u6D4B\u8BD5\u754C\u9762",-1),_=e("p",null,"\u5F53\u65B0\u5EFA\u4E00\u4E2Apromise\u5BF9\u8C61\u7684\u65F6\u5019\uFF0C\u4EC0\u4E48\u90FD\u4E0D\u505A\uFF0C\u5F97\u5230\u7684\u662F\u4E00\u4E2Apending\u5BF9\u8C61",-1),m=u("    "),E=u(`
+  `),f=a({setup(B){let o=new Promise((s,r)=>{});const t='[[Prototype]]: Promise[[PromiseState]]: "pending"[[PromiseResult]]: undefined';return l(()=>{console.log(o)}),(s,r)=>(c(),i(d,null,[u(n(F(o))+" ",1),p,_,e("pre",null,[m,e("code",null,`
+         `+n(t)+`
+
+    `),E])],64))}});export{f as default};
