35220cbd67189681895f6ab8fd81d6af0386ef6a
guo_hub <41369069+guohub8080@users.noreply.github.com>
2023-04-27 23:00:15 +0800

    Add files via upload

---
+++ src/router/routes.ts
@@ -0,0 +1,26 @@
+import {RouteRecordRaw} from "vue-router"
+import transcoding_router from "@/views/transcoding/router"
+
+const routes: Array<RouteRecordRaw> = [{
+    path: "/",
+    name: "default",
+    redirect: "/transcoding"
+},
+    transcoding_router
+    ,
+    {
+        path: "/test",
+        name: "test",
+        component: () => import("@/views/test/test.vue"),
+
+    },
+
+
+    {
+        path: "/:pathMatch(.*)*",
+        redirect: {name: "error_page"}
+    },
+
+]
+
+export default routes
---
+++ src/views/test/test.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+
+let p = new Promise((resolve, reject) => {
+
+})
+const l1 = "[[Prototype]]: Promise[[PromiseState]]: \"pending\"[[PromiseResult]]: undefined"
+onMounted(() => {
+  console.log(p)
+})
+</script>
+
+<template>
+  {{ p }}
+  <h1>这是测试界面</h1>
+  <p>当新建一个promise对象的时候，什么都不做，得到的是一个pending对象</p>
+  <pre>
+    <code>
+         {{l1}}
+
+    </code>
+  </pre>
+
+
+</template>
+
+<style scoped lang="less">
+
+</style>
